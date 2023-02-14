72ada064c99d98a08d264c6fe8d10d998c01eed7
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-02-14 09:47:04 +0100

    Make some changes in my main.js file.

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-class Awesome {
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-classes-per-file */
+class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
 
-// User interface class: Handle UI Tasks
+// UI Class: Handle UI Tasks
 class UI {
   static displayBooks() {
-    const books = store.getBooks();
+    const books = Store.getBooks();
 
-    books.forEach((book) => UI.addBookList(book));
+    books.forEach((book) => UI.addBookToList(book));
   }
-  static adddBookToList(book) {
+
+  static addBookToList(book) {
     const list = document.querySelector('#book-list');
 
     const bookDisplay = document.createElement('div');
-    bookDisplay.className ='bookList1';
-    bookDisplay.innerHTML = ` <p class="bookTitle"><b>${book.title}</b></p>
-      <p>by<span></span><b>${book.author}.</b></p>
-      <button class="delete">Remove</button>
-      `;
+    bookDisplay.className = 'bookList1';
+    bookDisplay.innerHTML = `
+        <p class="bookTitle"><b>${book.title}</b></p>
+        <p>by<span></span><b>${book.author}.</b></p>
+        <button class="delete">Remove</button>
+        `;
 
     list.appendChild(bookDisplay);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.remove();
+    }
   }
 
   static clearFields() {
@@ -31,10 +41,10 @@ class UI {
   }
 }
 
-//store Class: Handle storage
+// Store Class: Handles Storage
 class Store {
   static getBooks() {
-    let Books;
+    let books;
     if (localStorage.getItem('books') === null) {
       books = [];
     } else {
@@ -47,61 +57,62 @@ class Store {
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
-    localStorage.setItem('boooks', JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   static removeBook(title) {
-    const books = store.getBooks();
+    const books = Store.getBooks();
 
     books.forEach((book, index) => {
       if (book.title === title) {
         books.splice(index, 1);
       }
-    })
-    
+    });
+
     localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
-//Event: Display Books
+// Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-//Event: Add a Book
+// Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-//prevent actual submit
-e.preventDefault();
+  // Prevent actual submit
+  e.preventDefault();
 
-//Get form values 
-const title = document.querySelector('#title').value;
-const author = document.querySelector('#author').value;
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-//Instantiate book
-const book = new Book(title, author);
+  // Instatiate book
+  const book = new Book(title, author);
 
-// Add Book to UI
-UI.adddBookToList(book);
+  // Add Book to UI
+  UI.addBookToList(book);
 
-//Add book to store
-store.addBook(book);
+  // Add book to store
+  Store.addBook(book);
 
-// Clear fields
-UI.clearFields();
+  // Clear fields
+  UI.clearFields();
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-// Remove book from UI
-UI.deleteBook(e.target);
-// Remove book from store
-Store.removeBook(e.target.previousElementSiblings.previousElementSiblings.textContent);
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.previousElementSibling.previousElementSibling.textContent);
 });
 
 const currentDate = new Date().toLocaleString();
 document.getElementById('current-date').innerHTML = currentDate;
 
-//display the books list when click the button "List"
+// display the books list when click the button "List"
 const bookList = document.querySelector('.book-list-container');
-const listBtn = document.querySelector('.listBtn')
+const listBtn = document.querySelector('.listBtn');
 const formContainer = document.querySelector('.form-container');
 
 listBtn.addEventListener('click', () => {
@@ -115,7 +126,7 @@ window.addEventListener('load', () => {
   contactInfo.style.display = 'none';
 });
 
-//display the add book form when click then the button "Add new"
+// display the Add book form  when click the button "Add new"
 const addNewBtn = document.querySelector('.add-new-btn');
 
 addNewBtn.addEventListener('click', () => {
@@ -124,12 +135,12 @@ addNewBtn.addEventListener('click', () => {
   contactInfo.style.display = 'none';
 });
 
-// display the Contact section when click the button "Contact"
+// display the  Contact section when click the button "Contact"
 const contactBtn = document.querySelector('.contact');
 const contactInfo = document.querySelector('.contact-info');
 
 contactBtn.addEventListener('click', () => {
   bookList.style.display = 'none';
   formContainer.style.display = 'none';
-  contactInfo.style.display = 'block'
+  contactInfo.style.display = 'block';
 });
